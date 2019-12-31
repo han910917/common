a23ab00d96997993e09ebf026593ab6bc124ef6b
han910917 <han910917@gmail.com>
2019-12-31 18:00:04 +0800

    索引的简单查询

--- src/test/java/com/luence/common/CommonApplicationTests.java
+++ src/test/java/com/luence/common/CommonApplicationTests.java
@@ -27,9 +27,21 @@ class CommonApplicationTests {
         Field filedName = new TextField("name", "这个是luence测试使用的", Field.Store.YES);
         Field filedPrice = new StringField("price", "125.25", Field.Store.YES);
         Field filedAddress = new TextField("address", "广东省深圳市罗湖区仙湖植物园", Field.Store.YES);
+        Field filedName1 = new TextField("name", "这个不是luence测试使用的", Field.Store.YES);
+        Field filedPrice1 = new StringField("price", "125.25", Field.Store.YES);
+        Field filedAddress1 = new TextField("address", "深圳罗湖区仙湖植物园", Field.Store.YES);
+        Field filedName2 = new TextField("name", "这个也不是luence测试使用的", Field.Store.YES);
+        Field filedPrice2 = new StringField("price", "125.25", Field.Store.YES);
+        Field filedAddress2 = new TextField("address", "深圳市罗湖区仙湖植物园", Field.Store.YES);
         list.add(filedName);
         list.add(filedPrice);
         list.add(filedAddress);
+        list.add(filedName1);
+        list.add(filedPrice1);
+        list.add(filedAddress1);
+        list.add(filedName2);
+        list.add(filedPrice2);
+        list.add(filedAddress2);
         Object json = null;
         try {
             json = LuenceInfo.class.newInstance().indexCreate(list);
@@ -45,7 +57,7 @@ class CommonApplicationTests {
     @Test
     void indexSearch(){
         try {
-            List<Map<String, Object>> list = LuenceInfo.class.newInstance().indexSearch("深圳市");
+            List<Map<String, Object>> list = LuenceInfo.class.newInstance().indexSearch("深圳");
             if(CollectionUtils.isEmpty(list)) System.out.println("未查询到数据");
             for (Map<String, Object> map : list) {
                 System.out.println( "name = "+map.get("name") + " price = "+map.get("price") + " address = "+map.get("address") );
